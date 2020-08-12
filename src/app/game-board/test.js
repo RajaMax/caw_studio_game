15dec92505b0df0a33df72769137456cdfb53b20
rajaabinesh <rajaabineshc@gmail.com>
2020-08-12 08:34:27 +0530

    desxr added

--- src/app/game-board/test.js
+++ src/app/game-board/test.js
@@ -1,16 +1,20 @@
-function charCount(str){
-	var a=str.split('');
-	a.forEach(funtion(s){
-	var count=0;
-	var obj={}
-	for(var j=0;j<a.length;j++){
-		if(s==a[j]){
-		count++;
-	}
-		obj[s]=count;
-	}
-	});
-	return obj
+function charCount(str) {
+    var a = str.split('');
+    var obj = {}
+    a.forEach(function(s){
+        var count = 0
+	for(var j = 0; j<a.length; j++){
+        if (s == a[j]) {
+            count++;
+        }
+        obj[s] = count;
+    }
+});
+var output =''
+for(var key in obj){
+    output=output+key+obj[key]
+}
+return output
 
 	
 }
